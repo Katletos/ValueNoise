97a9b97d776d3c70d892c18d72a3246368b6a6d0
Mikita <40485432+Katletos@users.noreply.github.com>
2022-11-11 14:33:56 +0300

    Add files via upload

--- Main.asm
+++ Main.asm
@@ -1,45 +1,52 @@
 format PE GUI 4.0
 entry WinMain
 include 'win32w.inc'
-        
+
+
+proc DemoWindow.WindowProc uses ebx esi edi,\
+     hWnd, uMsg, wParam, lParam
+     cmp     [uMsg], WM_DESTROY
+     je      .WMDestroy
+.Default:
+     invoke  DefWindowProc, [hWnd], [uMsg], [wParam], [lParam]
+     jmp     .EndProc
+.WMDestroy:
+     invoke  PostQuitMessage, 0
+     jmp     .Default
+.EndProc:
+     ret
+endp
+
+
+
 proc WinMain
      locals
         Msg             MSG
         pD3D            dd      ?
         pD3DDevice      dd      ?
-        EDIT            du  'EDIT', 0
      endl
 
-     ;Use standard lpClassName
      xor     ebx, ebx
-     lea edx, [EDIT]
-     ;Create window
-     invoke  CreateWindowEx, ebx, edx, ebx, WS_POPUP or WS_MAXIMIZE or WS_VISIBLE,\
-                             ebx, ebx, ebx, ebx, ebx, ebx, ebx, ebx
-     ;Hide cursor
+     invoke  RegisterClassEx, DemoWindow.wcexClass
      invoke  ShowCursor, ebx
+     invoke  CreateWindowExW, ebx, DemoWindow.szClassName, ebx, WS_POPUP or WS_VISIBLE or WS_MAXIMIZE,\
+                             ebx, ebx, ebx, ebx, ebx, ebx, ebx, ebx
 
      mov     esi, eax
      invoke  Direct3DCreate9, D3D_SDK_VERSION
      mov     [pD3D], eax
      lea     edx, [pD3DDevice]
-;     comcall eax, IDirect3D9, CreateDevice, D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, esi,\
-;                                            D3DCREATE_HARDWARE_VERTEXPROCESSING, d3dppDemo,\
-;                                            edx
-
-     ;Chose vertex format
-;     comcall    [pD3DDevice], IDirect3DDevice9, SetFVF, D3DFVF_XYZRHW
-
+     comcall eax, IDirect3D9, CreateDevice, D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, esi,\
+                                            D3DCREATE_HARDWARE_VERTEXPROCESSING, d3dppDemo,\
+                                            edx
      lea     edi, [Msg]
 .MsgLoop:
      invoke  PeekMessage, edi, ebx, ebx, ebx, PM_REMOVE
      test    eax, eax
      jnz     .ProcessMessage
-;     comcall [pD3DDevice], IDirect3DDevice9, BeginScene
-;     comcall [pD3DDevice], IDirect3DDevice9, DrawPrimitiveUP,\
-;                                           D3DPT_TRIANGLELIST, 2, Quad, sizeof.TVertex
-;     comcall [pD3DDevice], IDirect3DDevice9, Present, ebx, ebx, ebx, ebx
-;     comcall [pD3DDevice], IDirect3DDevice9, EndScene
+     invoke  GetTickCount
+     comcall [pD3DDevice], IDirect3DDevice9, Clear, ebx, ebx, D3DCLEAR_TARGET, eax, ebx, ebx
+     comcall [pD3DDevice], IDirect3DDevice9, Present, ebx, ebx, ebx, ebx
      jmp     .MsgLoop
 .ProcessMessage:
      cmp     [Msg.message], WM_QUIT
@@ -55,23 +62,6 @@ endp
 
 
 data import
-
-                  struct TVertex
-       x        dd      ?
-       y        dd      ?
-       z        dd      ?
-       rhw      dd      ?
-     ends
-
-      Quad   TVertex                 100.0, 100.0, 0.0, 1.0
-       TVertex                 200.0, 100.0, 0.0, 1.0
-       TVertex                 200.0, 200.0, 0.0, 1.0
-       TVertex                 200.0, 200.0, 0.0, 1.0
-       TVertex                 100.0, 200.0, 0.0, 1.0
-       TVertex                 100.0, 100.0, 0.0, 1.0
-
-
-
         library kernel32, 'kernel32.dll',\
                 gdi32,    'gdi32.dll',\
                 user32,   'user32.dll',\
@@ -83,4 +73,15 @@ data import
         include '%myinclude%\API\d3d9.inc'
         
         include '%myinclude%\EQUATES\d3d9.inc'
+
 end data
+
+
+
+ImageBase = $ - rva $
+nil       = 0
+
+DemoWindow.szClassName    du              'Demo', 0
+DemoWindow.wcexClass      WNDCLASSEX      sizeof.WNDCLASSEX, CS_GLOBALCLASS,\
+                                          DemoWindow.WindowProc, 0, 0, ImageBase,\
+                                          0, 0, 0, nil, DemoWindow.szClassName, 0
